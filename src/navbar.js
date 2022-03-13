25888b9e846025ec9b4fa3d873d39cd61492b4df
Yi Guo <guoyi0328@gmail.com>
2022-03-12 20:50:32 -0800

    add navbar and footer

---
+++ src/navbar.js
@@ -0,0 +1,34 @@
+import "./navbar.css";
+
+function Navbar() {
+    return (
+        <nav>
+            <div className="container">
+                <div id="name-block">
+                    <a href="#home">
+                        <div id="name">Yi Guo</div>
+                        <div id="affiliation">PhD, UC San Diego</div>
+                    </a>
+                </div>
+                <div id="menu-block">
+                    <ul>
+                        <li>
+                            <a href="#research">Research</a>
+                        </li>
+                        <li>
+                            <a href="#interest">Interest</a>
+                        </li>
+                        <li>
+                            <a href="#history">History</a>
+                        </li>
+                        <li>
+                            <a href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
